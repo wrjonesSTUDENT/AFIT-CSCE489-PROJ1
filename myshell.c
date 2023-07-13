9da1aaeb8e5e957bc287d2f234e7989bb4872c5d
wrjonesSTUDENT <wrjonesSTUDENT@gmail.com>
2023-07-12 20:37:01 -0400

    5th commit TODO input validation? 1. create - need to figure out how to pass arguments 2. update - 0% 3. list - 0% 4. dir - complete 5. halt - complete 6. help - complete 7. banner - complete
    buffer works but is deliminated by spaces so i need to figure out how to handle long strings in quotes i guess
    
    buffer[] - raw commands typed by user
    userInput[] - array of inputs deliminated by spaces "i think"
    bufferToCommand
    
    Sample Input
    The sample input below is a sequence of correctly formatted commands. Your program should detect and ignore commands that are not correctly formatted.
    
    dir
    create file1
    dir
    update file1 3 “this is a line”
    list file1
    update file1 2 “this is another line”
    list file1
    create file2
    update file2 10 “this is the first 10 lines” &
    list file2
    create file3
    dir
    list file2
    update file3 10 “first update” &
    update file3 12 “second update”
    list file3
    list file4
    dir
    halt

--- myshell.c
+++ myshell.c
@@ -9,32 +9,32 @@
 #include <string.h>
 #include <unistd.h>
 
-void bufferToCommand(char userInput[25]) {
-    if (strcmp(userInput, "dir") == 0) {
-        //printf("dir command sent\n");
-        dir();
-    } else if (strcmp(userInput, "update") == 0) {
-        printf("update command sent\n");
-    } else if (strcmp(userInput, "list") == 0) {
-        printf("list command sent\n");
-    } else if (strcmp(userInput, "halt") == 0) {
-        printf("roscoe shell exited\n");
-        halt();
-    } else if (strcmp(userInput, "create") == 0) {
-        printf("create command sent\n");
-    } else if (strcmp(userInput, "help") == 0) {
-        //printf("help command sent\n");
-        help();
-    } else if (strcmp(userInput, "banner") == 0) {
-        banner();
-    } else {
-        printf("invalid command, try entering \"help\" for available commands\n");
-    }
-}
+//void bufferToCommand(char userInput[25]) {
+//    if (strcmp(userInput, "dir") == 0) {
+//        //printf("dir command sent\n");
+//        dir();
+//    } else if (strcmp(userInput, "update") == 0) {
+//        printf("update command sent\n");
+//    } else if (strcmp(userInput, "list") == 0) {
+//        printf("list command sent\n");
+//    } else if (strcmp(userInput, "halt") == 0) {
+//        printf("roscoe shell exited\n");
+//        halt();
+//    } else if (strcmp(userInput, "create") == 0) {
+//        printf("create command sent\n");
+//    } else if (strcmp(userInput, "help") == 0) {
+//        //printf("help command sent\n");
+//        help();
+//    } else if (strcmp(userInput, "banner") == 0) {
+//        banner();
+//    } else {
+//        printf("invalid command, try entering \"help\" for available commands\n");
+//    }
+//}
 int main() {
-    char buffer[25];
-    char userInput[25];
-    banner();
+    char buffer[256];
+    char userInput[6][256];
+//    banner();
     while (1) {
         printf("Roscoe: ");
         fgets(buffer, 256, stdin);
@@ -45,9 +45,11 @@ int main() {
         }
 
         int i = 0;
-        while (sscanf(buffer + i, "%s", userInput) == 1) {
-            bufferToCommand(userInput);
-            i += strlen(userInput) +1;
-    }
+        int j = 0;
+        while (j < length){
+            sscanf(buffer + i, "%s", userInput[j]);
+            i += strlen(userInput[j]) +1;
+            j++;
+            }
     }
 }
