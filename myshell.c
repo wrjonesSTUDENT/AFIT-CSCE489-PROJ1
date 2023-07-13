858f4410ff0667575e925b08e724d2cdfdcca9ac
wrjonesSTUDENT <wrjonesSTUDENT@gmail.com>
2023-07-12 21:15:48 -0400

    6th commit BEFORE I BREAK IT TODO input validation? 1. create - need to figure out how to pass arguments 2. update - 0% 3. list - 0% 4. dir - complete 5. halt - complete 6. help - complete 7. banner - complete
    buffer works but is deliminated by spaces so i need to figure out how to handle long strings in quotes i guess
    
    buffer[] - raw commands typed by user
    userInput[] - array of inputs deliminated by spaces "i think"
    bufferToCommand
    
    Sample Input
    The sample input below is a sequence of correctly formatted commands. Your program should detect and ignore commands that are not correctly formatted.
    
    dir
    create file1
    dir
    update file1 3 “this is a line”
    list file1
    update file1 2 “this is another line”
    list file1
    create file2
    update file2 10 “this is the first 10 lines” &
    list file2
    create file3
    dir
    list file2
    update file3 10 “first update” &
    update file3 12 “second update”
    list file3
    list file4
    dir
    halt

--- myshell.c
+++ myshell.c
@@ -34,22 +34,31 @@
 int main() {
     char buffer[256];
     char userInput[6][256];
+    // char quotedArg[256];
 //    banner();
     while (1) {
         printf("Roscoe: ");
+        //reads stdin stream and puts in a character buffer
         fgets(buffer, 256, stdin);
-
+        //measure the length of the buffer array and find the carriage return and replace with a \0
         int length = strlen(buffer);
         if ((buffer[length -1 ]) == '\n') {
             buffer[length - 1] = '\0';
         }
-
+        // place each string from the buffer array into the userInput array row each time a space is encountered
         int i = 0;
         int j = 0;
-        while (j < length){
-            sscanf(buffer + i, "%s", userInput[j]);
+        while (j < length){ //this isn't the greatest indicator of how many times i should loop through
+            sscanf(buffer + i, "%[A-Z,a-z,0-9,&]s", userInput[j]);
             i += strlen(userInput[j]) +1;
             j++;
             }
     }
+//       failed attempt at capturing the arg in quotes
+//        if (sscanf(buffer, "\"%[^\"]\"", quotedArg) ==1) {
+//            printf("quoted content %s\n", quotedArg);
+//        } else {
+//            printf("No quoted content %s\n");
+//            break;
+//        }
 }
