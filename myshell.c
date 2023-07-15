7ed2b03ca0585040aa9483731b47054d3114e907
wrjonesSTUDENT <wrjonesSTUDENT@gmail.com>
2023-07-15 15:16:59 -0400

    TODO input validation? create a working dir? list limitations
    1. create - complete
    2. update - 100%
    3. list - 99%
    4. dir - complete
    5. halt - complete
    6. help - complete
    7. banner - complete
    8. background a command - 0%
    
    Limitations
    1. any command can only have 5 args because of the way i took in user input
    2. must launch the shell from whatever directoy you want to work in because there is no cd command
    
    buffer works but is deliminated by spaces so i need to figure out how to handle long strings in quotes i guess
    
    buffer[] - raw commands typed by user
    userInput[] - array of inputs deliminated by spaces "i think"
    bufferToCommand
    
    Sample Input
    The sample input below is a sequence of correctly formatted commands. Your program should detect and ignore commands that are not correctly formatted.
    %*[^"]"%[^"]"
    dir
    create file1
    dir
    update file1 3 “this is a line”
    list file1
    update file1 2 “this is another line”
    list file1
    create file2
    update file2 10 “this is the first 10 lines” &
    list file2
    create file3
    dir
    list file2
    update file3 10 “first update” &
    update file3 12 “second update”
    list file3
    list file4
    dir
    halt

--- myshell.c
+++ myshell.c
@@ -8,39 +8,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-void bufferToCommand(char userCmd[256], char arg1[256], char arg2[256], char arg3[256], char arg4[256], char arg5[256], char quotedString[256]) {
-    if (strcmp(userCmd, "dir") == 0) {
-        //printf("dir command sent\n");
-        dir();
-    } else if (strcmp(userCmd, "update") == 0) {
-//        printf("update command sent\n");
-//        printf("print the array arg2 %s\n", arg2);
-//        printf("print the array[0] %d\n", arg2[0]);
-        update(arg1, arg2, quotedString);
-    } else if (strcmp(userCmd, "list") == 0) {
-        list(arg1);
-    } else if (strcmp(userCmd, "halt") == 0) {
-        halt();
-    } else if (strcmp(userCmd, "create") == 0) {
-        create(arg1);
-    } else if (strcmp(userCmd, "help") == 0) {
-        help();
-    } else if (strcmp(userCmd, "banner") == 0) {
-        banner();
-    } else {
-        printf("invalid command, try entering \"help\" for available commands\n");
-    }
-}
+//void bufferToCommand(char userCmd[256], char arg1[256], char arg2[256], char arg3[256], char arg4[256], char arg5[256], char quotedString[256]) {
+//    if (strcmp(userCmd, "dir") == 0) {
+//        //printf("dir command sent\n");
+//        dir();
+//    } else if (strcmp(userCmd, "update") == 0) {
+////        printf("update command sent\n");
+////        printf("print the array arg2 %s\n", arg2);
+////        printf("print the array[0] %d\n", arg2[0]);
+//        update(arg1, arg2, quotedString);
+//    } else if (strcmp(userCmd, "list") == 0) {
+//        list(arg1);
+//    } else if (strcmp(userCmd, "halt") == 0) {
+//        halt();
+//    } else if (strcmp(userCmd, "create") == 0) {
+//        create(arg1);
+//    } else if (strcmp(userCmd, "help") == 0) {
+//        help();
+//    } else if (strcmp(userCmd, "banner") == 0) {
+//        banner();
+//    } else {
+//        printf("invalid command, try entering \"help\" for available commands\n");
+//    }
+//}
 int main() {
     char buffer[256];
-    banner();
+//    banner();
+    bool background = false;
     while (1) {
         printf("Roscoe: ");
         //reads stdin stream and puts in a character buffer
         fgets(buffer, 256, stdin);
         //measure the length of the buffer array and find the carriage return and replace with a \0
         int length = strlen(buffer);
+        if ((buffer[length - 2 ]) == '&') {
+                background = true; }
         if ((buffer[length -1 ]) == '\n') {
             buffer[length - 1] = '\0';
         }
@@ -57,7 +61,7 @@ int main() {
         sscanf(buffer, "%*[^\"\n]\"%[^\"]\")", quotedString);
         //break up the buffer each time there is a space
         sscanf(buffer, "%s %s %s %s %s %s", userCmd, arg1, arg2, arg3, arg4, arg5);
-        bufferToCommand(userCmd, arg1, arg2, arg3, arg4, arg5, quotedString);
-        clearInputArrays(userCmd, arg1, arg2, arg3, arg4, arg5, quotedString);
+//        bufferToCommand(userCmd, arg1, arg2, arg3, arg4, arg5, quotedString);
+//        clearInputArrays(userCmd, arg1, arg2, arg3, arg4, arg5, quotedString);
         }
       }
